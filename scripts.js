b34149f41e8f2e92424ab334b2fae03af31a33b8
lmccraw123 <laurenmccraw@gmail.com>
2021-02-04 19:53:02 -0800

    init commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+//allow the user to enter their guest
+let num = prompt('Guess a number between 1 and 10');
+// check to see if the numer is a winner
+if (num == 5)
+    // If it is, they got it right
+    {alert("You Guessed the Right number");}
+else
+    //if it isnt, they got it wrong
+    {alert("You Guessed the Wrong Number");}
+
+        
+  
